43811f3ddece27aa0c127266a05594e38af9f605
Prem <kumarprem10694@gmail.com>
2025-02-05 17:21:11 +0530

    Exception handling

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,19 @@
 import sys
 
 def error_message_details(error, error_detail :sys) -> str:
-    _,_,error_message = error_detail.exc_info()
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename 
     
+    error_message = " Error occured in python script name [{0}] line number [{1}] error message [{2}] ".format(
+        file_name , exc_tb.tb_lineno , str(error)
+   )
     
+    return error_message
+
+class CustomException(Exception):
+    def __init(self , error_message , error_details :sys) ->None:
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message , error_details)
+        
+    def  __str__(self) -> str:
+        return self.error_message    
